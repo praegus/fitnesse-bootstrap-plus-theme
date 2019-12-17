084a5266a6c6281c52b86146686c9fc16f6e6a4b
alex pennings <apennings@roc-dev.com>
2019-12-17 11:34:15 +0100

    changed functionality to check page length, this has to do with styling. it also checks if the page is an edit page, this is because the edit page doesnt contain an footer and can herefore make an error.

--- fitnesse/resources/bootstrap-plus/js/bootstrap-plus-gold.js
+++ fitnesse/resources/bootstrap-plus/js/bootstrap-plus-gold.js
@@ -10,12 +10,23 @@ $(document).ready(function() {
     });
 });
 // an array is made with the csv text,split then a random tooltip is chosen and displayed as injected DOM element in the body
+//the function also checks if the page in question is the page with the editor since that page doesnt contain a footer
+// after that it checks what the length of the page is, so it can display the tooltip at the appropriate position.
 function displayToolTip(text) {
     var textarray = text.split(",");
     var PickedTip = Math.floor(Math.random() * textarray.length);
-    var body = document.querySelector('body');
+    var article = document.querySelector('article');
     var tooltip = document.createElement("div");
-    body.appendChild(tooltip);
+    var body = document.querySelector('body');
+    console.log(body.className);
+    if(article.offsetHeight < 1000 && body.className != "editPage"){
+        var parent = document.querySelector('footer');
+        tooltip.setAttribute("style","margin-top: -4%;text-align: center;");
+    }else{
+        var parent = body;
+        tooltip.setAttribute("style","width:400px;margin:-1% auto;height:20px;margin-top:22px;text-align:center");
+}
+    parent.appendChild(tooltip);
     tooltip.id = "tooltip";
     tooltip.innerHTML = '<img src="/files/fitnesse/bootstrap-plus/img/hint.png"><p id="tooltip">' + textarray[PickedTip] + '</p>';
 }
