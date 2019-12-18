60e070942f7ea808e7c0f50678fc6d6af5193649
alex pennings <apennings@roc-dev.com>
2019-12-18 08:41:35 +0100

    styling update

--- fitnesse/resources/bootstrap-plus/js/bootstrap-plus-gold.js
+++ fitnesse/resources/bootstrap-plus/js/bootstrap-plus-gold.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
 // an array is made with the csv text,split then a random tooltip is chosen and displayed as injected DOM element in the body
 //the function also checks if the page in question is the page with the editor since that page doesnt contain a footer
 // after that it checks what the length of the page is, so it can display the tooltip at the appropriate position.
+/*
 function displayToolTip(text) {
     var textarray = text.split(",");
     var PickedTip = Math.floor(Math.random() * textarray.length);
@@ -29,4 +30,17 @@ function displayToolTip(text) {
     parent.appendChild(tooltip);
     tooltip.id = "tooltip";
     tooltip.innerHTML = '<img src="/files/fitnesse/bootstrap-plus/img/hint.png"><p id="tooltip">' + textarray[PickedTip] + '</p>';
+}
+
+ */
+function displayToolTip(text) {
+    var textarray = text.split(",");
+    var PickedTip = Math.floor(Math.random() * textarray.length);
+    var article = document.querySelector('article');
+    var tooltip = document.createElement("div");
+    var parent = document.querySelector('body');
+    parent.appendChild(tooltip);
+    tooltip.id = "tooltip";
+    tooltip.setAttribute('style','margin-top: 7%;text-align: center;')
+    tooltip.innerHTML = '<img style="position:relative;top:30px:" src="/files/fitnesse/bootstrap-plus/img/hint.png"><p style="position:relative;top:30px:" id="tooltip">' + textarray[PickedTip] + '</p>';
 }
