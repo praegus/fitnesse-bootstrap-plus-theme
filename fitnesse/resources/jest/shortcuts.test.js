e22f5dcc5cbe51c0e7e13186de4fb2cc66492d15
alex pennings <apennings@roc-dev.com>
2019-12-31 12:13:38 +0100

    adjusted unit testing

--- fitnesse/resources/jest/shortcuts.test.js
+++ fitnesse/resources/jest/shortcuts.test.js
@@ -9,4 +9,5 @@ test('no duplicate elements',()=>{
         '<ul class="help-list">';
     // expect the length of all DOM elements with the class "help-list" wich is 2
  expect(shortcut.Createshortcuts(key)).toBe(2);
+ expect(shortcut.Createshortcuts("urlfound")).toBe(2);
 });
