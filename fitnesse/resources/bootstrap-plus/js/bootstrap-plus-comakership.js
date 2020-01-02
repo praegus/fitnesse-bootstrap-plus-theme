f4d7f612f228bc8d195d85149094818ca15cfe41
alex pennings <apennings@roc-dev.com>
2020-01-02 14:11:07 +0100

    changed method so it uses velocity templates

--- fitnesse/resources/bootstrap-plus/js/bootstrap-plus-comakership.js
+++ fitnesse/resources/bootstrap-plus/js/bootstrap-plus-comakership.js
@@ -1,45 +1,25 @@
 
 // if the document has been loaded, then get data from toolTipData.csv
-function getTooltips()
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "files/fitnesse/bootstrap-plus/csv/toolTipData.csv",false); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-// gets article height
-function getArticleHeight(){
-    if (document.querySelector('article') === undefined) {
-    } else {
-        var article = document.querySelector('article');
-        return article.offsetHeight;
-    }
-}
+function getToolTips(callback){
+    var tooltips;
+    $.get("files/fitnesse/bootstrap-plus/csv/toolTipData.csv",function(data){
+        tooltips = data;
+        callback(tooltips);
+    });
 
-function displayToolTip(text, height) {
+}
+function displayToolTip(text) {
     //initialization code
     var textarray = text.split(",");
     var PickedTip = Math.floor(Math.random() * textarray.length);
-    var tooltip = document.createElement("div");
-    var parent = document.querySelector('body');
-    //checks if current page is an editpage
+    var textfield = document.getElementById("tooltip");
+    textfield.innerHTML = textarray[PickedTip];
 
-        tooltip.setAttribute('style','text-align: center;');
-
-    //creates DOM element and appends it
-    parent.appendChild(tooltip);
-    tooltip.id = "tooltip";
-    tooltip.innerHTML = '<img style="position:relative;" src="/files/fitnesse/bootstrap-plus/img/hint.png"><p style="display:inline;position:relative;" id="tooltip">' + textarray[PickedTip] + '</p>';
-    //return for unit-testing
-    return tooltip.getAttribute('style') + PickedTip + "," + textarray[PickedTip];
-}
-// callback makes sure toolTip function receives data at the same time
-function callback(callback,param1,param2){
-    callback(param1,param2);
+    return PickedTip+","+textarray[PickedTip];
 }
 //executes callback function when document is ready
 $(document).ready(function () {
-    callback(displayToolTip,getTooltips(),getArticleHeight());
+    getToolTips(displayToolTip);
 });
 
 try{
--- fitnesse/resources/jest/Tooltip.test.js
+++ fitnesse/resources/jest/Tooltip.test.js
@@ -1,26 +1,16 @@
 const tooltip = require('bootstrap-plus/js/bootstrap-plus-comakership');
-
-//tests if DOM element is correct by giving different MOCK article heights
-test('tooltip Correct DOM element test', () => {
-    expect(tooltip.displayToolTip("text",200)).toBe("position:relative;top:520px;text-align:center;0,text");
-    expect(tooltip.displayToolTip("text",300)).toBe("position:relative;top:482px;text-align:center;0,text");
-    expect(tooltip.displayToolTip("text",600)).toBe("margin-top: 8%;text-align: center;0,text");
-    //checks if editpage is being detected and the tooltip is adjusted accordingly
-    var body = document.querySelector('body');
-    body.className = "editPage";
-    expect(tooltip.displayToolTip("text",600)).toBe("margin-top: 1%;text-align: center;0,text");
-});
 //checks if inputted CSV format data comes out correctly
 test('tooltip Correct data randomization test', () => {
+    document.body.innerHTML=
+        '<p id="tooltip">'+'</p>';
+
     //init code
     var text = "text1,text2,text3";
     var textarray = text.split(",");
     //splits off result from other test
-    var result = tooltip.displayToolTip(text);
-    var seperatedresult = result.split(";");
+    var result = tooltip.displayToolTip(text)
     //splits usable result
-    var seperatedresult2 = seperatedresult[2].split(",");
-    //checks if usable result is correct
-    expect(textarray[seperatedresult2[0]]).toBe(seperatedresult2[1]);
+    var seperatedresult = result[2].split(",");
+    expect(textarray[seperatedresult[0]]).toBe(seperatedresult[1]);
 });
 
