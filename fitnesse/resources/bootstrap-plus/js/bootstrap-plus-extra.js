ed16f75eed668dd8000dc765082f779127aa09ce
alex pennings <apennings@roc-dev.com>
2020-01-02 16:44:39 +0100

    working on getting current version number

---
+++ fitnesse/resources/bootstrap-plus/js/bootstrap-plus-extra.js
@@ -0,0 +1,25 @@
+
+$(document).ready(getVersionData);
+function getVersionData(callback){
+    var versions;
+ $.get("https://api.github.com/repos/unclebob/fitnesse/tags",function(data){
+     versions = data;
+     callback(versions);
+    });
+
+}
+function versionCheck(data) {
+    var versions = []
+   for (var i=0; i < data.length; i++){
+       data[i].name = data[i].name.replace(/\D/g,'');
+       if (data[i].name == ""){
+           data.splice(i,1);
+       }
+       data[i].name = parseInt(data[i].name);
+       versions.push(data[i].name);
+   }
+  
+
+   console.log(versions[0]);
+}
+getVersionData(versionCheck);
