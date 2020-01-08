f47eec7f5a8a8059e890bcb3bb1c0ecdfa7296c2
alex pennings <apennings@roc-dev.com>
2020-01-08 15:57:53 +0100

    made callbacks asynchronus

--- fitnesse/resources/bootstrap-plus/js/bootstrap-plus-extra.js
+++ fitnesse/resources/bootstrap-plus/js/bootstrap-plus-extra.js
@@ -1,32 +1,46 @@
 
-$(document).ready(getVersionData,getCurrentVersion);
+
 function getVersionData(callback){
     var versions;
+    var currentversion
     $.get("https://api.github.com/repos/unclebob/fitnesse/tags",function(data) {
         versions = data;
-        callback(versions);
-    })}
-function getCurrentVersion(callback){
-    var versions;
-    $.get("localhost:9090/?fitnesseVersion",function(data) {
+
+callback(versions);
+    })
+    $.get("http://localhost:9090/?fitNesseVersion",function(data) {
         currentversion = data;
-        console.log(data);
         callback(currentversion);
-    })}
 
 
-function versionCheck(data,currentversion) {
-    var versions = []
-   for (var i=0; i < data.length; i++){
-       data[i].name = data[i].name.replace(/\D/g,'');
-       if (data[i].name == ""){
-           data.splice(i,1);
-       }
-       data[i].name = parseInt(data[i].name);
-       versions.push(data[i].name);
-   }
-  versions.sort();
+    })
 
-   console.log(versions);
 }
+var newestversion = null;
+function versionCheck(data) {
+if (newestversion == null) {
+
+    var versions = []
+    for (var i = 0; i < data.length; i++) {
+        data[i].name = data[i].name.replace(/\D/g, '');
+        if (data[i].name == "") {
+            data.splice(i, 1);
+        }
+        data[i].name = parseInt(data[i].name);
+        versions.push(data[i].name);
+    }
+    versions.sort();
+    versions.reverse();
+
+    console.log(versions[0]);
+    newestversion = versions[0];
+}else {
+    var checkDOM = document.getElementById("versioncheck");
+    if (newestversion != data){
+       checkDOM.innerHTML = "<p>Release v"+data+"</p><p> - Out of date</p>"
+
+    }
+}
+}
+
 getVersionData(versionCheck);
