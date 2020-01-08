1b36ea5a58a36a3bf07cb091259b6300e031300e
alex pennings <apennings@roc-dev.com>
2020-01-08 14:15:51 +0100

    added getter for current version data

--- fitnesse/resources/bootstrap-plus/js/bootstrap-plus-extra.js
+++ fitnesse/resources/bootstrap-plus/js/bootstrap-plus-extra.js
@@ -1,20 +1,21 @@
 
-$(document).ready(getVersionData);
+$(document).ready(getVersionData,getCurrentVersion);
 function getVersionData(callback){
     var versions;
- $.get("https://api.github.com/repos/unclebob/fitnesse/tags",function(data){
-     versions = data;
-     callback(versions);
-    });
+    $.get("https://api.github.com/repos/unclebob/fitnesse/tags",function(data) {
+        versions = data;
+        callback(versions);
+    })}
+function getCurrentVersion(callback){
+    var versions;
+    $.get("localhost:9090/?fitnesseVersion",function(data) {
+        currentversion = data;
+        console.log(data);
+        callback(currentversion);
+    })}
 
-}
 
-$.get("prop",function(data){
-    console.log(data);
-});
-
-}
-function versionCheck(data) {
+function versionCheck(data,currentversion) {
     var versions = []
    for (var i=0; i < data.length; i++){
        data[i].name = data[i].name.replace(/\D/g,'');
@@ -24,8 +25,8 @@ function versionCheck(data) {
        data[i].name = parseInt(data[i].name);
        versions.push(data[i].name);
    }
-  
+  versions.sort();
 
-   console.log(versions[0]);
+   console.log(versions);
 }
 getVersionData(versionCheck);
