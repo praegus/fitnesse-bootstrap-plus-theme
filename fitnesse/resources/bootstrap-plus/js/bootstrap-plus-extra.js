b77652d00f36afcd0455c81722debbd473f84caf
alex pennings <apennings@roc-dev.com>
2020-01-13 14:35:24 +0100

    changed url generation method

--- fitnesse/resources/bootstrap-plus/js/bootstrap-plus-extra.js
+++ fitnesse/resources/bootstrap-plus/js/bootstrap-plus-extra.js
@@ -1,3 +1,19 @@
 $(document).ready(
-    $.get()
+    $.get("http://localhost:9090/?testHistory", function (data) {
+        var parser = new DOMParser();
+        var testhistory = parser.parseFromString(data,'text/html');
+        testhistory = testhistory.getElementsByTagName("table")[0];
+        var tablerows = testhistory.getElementsByTagName("th");
+        for (var i = 1;i<tablerows.length; i++) {
+               var tablerowdate = tablerows[i].childNodes[3];
+               var tablerowdate = tablerowdate.innerHTML.replace(/\D+/g,"").replace(",","").replace(":","").replace(/\ /g,"");
+     console.log(tablerowdate);
+        }
+
+
+        document.body.appendChild(testhistory);
+
+        console.log(testhistory);
+
+    })
 )
