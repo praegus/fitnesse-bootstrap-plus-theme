cca0a4005bc56d1655991e815281567b4350a75a
alex pennings <apennings@roc-dev.com>
2020-01-21 10:30:45 +0100

    added documentaqtion, formated code

--- fitnesse/resources/jest/Versiecheck.test.js
+++ fitnesse/resources/jest/Versiecheck.test.js
@@ -1,18 +1,17 @@
 const versiecheck = require('bootstrap-plus/js/bootstrap-plus-extra');
 describe('Versiechecker', function () {
+    //mock table in innerhtml
     document.body.innerHTML = '<table id="versioncheck"><tbody></tbody></table>';
-
     test('expect current version to be up to date', () => {
+        // test if version up to date
         expect(versiecheck.versionCheck(2,2,"testapp")).toBe("up-to-date")
     });
-
     test('expect current version to be outdated', () => {
+        // test if version outdated
         expect(versiecheck.versionCheck(2,1,"testapp")).toBe("outdated")
     });
-
     test('expect current version to be too high', () => {
+        // test if version too high
         expect(versiecheck.versionCheck(2,3,"testapp")).toBe("current version is to high")
     });
-
-
 });
