6f70b2df3a2d2973df7bf152d7b833740936b9fa
alex pennings <apennings@roc-dev.com>
2020-01-21 11:38:26 +0100

    css edit

--- fitnesse/resources/bootstrap-plus/js/bootstrap-plus-extra.js
+++ fitnesse/resources/bootstrap-plus/js/bootstrap-plus-extra.js
@@ -1,19 +1,8 @@
 $(document).ready(
-    $.get("http://localhost:9090/?testHistory", function (data) {
-        var parser = new DOMParser();
-        var testhistory = parser.parseFromString(data,'text/html');
-        testhistory = testhistory.getElementsByTagName("table")[0];
-        var tablerows = testhistory.getElementsByTagName("th");
-        for (var i = 1;i<tablerows.length; i++) {
-               var tablerowdate = tablerows[i].childNodes[3];
-               var tablerowdate = tablerowdate.innerHTML.replace(/\D+/g,"").replace(",","").replace(":","").replace(/\ /g,"");
-     console.log(tablerowdate);
-        }
+    $.get("http://localhost:9090/recentChanges", function (data) {
+      console.log(data);
 
 
-        document.body.appendChild(testhistory);
-
-        console.log(testhistory);
 
     })
-)
+);
