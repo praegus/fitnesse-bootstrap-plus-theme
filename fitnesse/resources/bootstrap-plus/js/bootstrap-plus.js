9cb834ec9229bad60b37cf0de4459cf0b8f59b08
Alex <s1129351@student.windesheim.nl>
2020-01-24 12:16:13 +0100

    Reformat code

--- fitnesse/resources/bootstrap-plus/js/bootstrap-plus.js
+++ fitnesse/resources/bootstrap-plus/js/bootstrap-plus.js
@@ -1,8 +1,8 @@
-String.prototype.UcFirst = function () {
+String.prototype.UcFirst = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
-String.prototype.replaceAll = function (search, replacement) {
+String.prototype.replaceAll = function(search, replacement) {
     var target = this;
     return target.replace(new RegExp(search, 'g'), replacement);
 };
@@ -38,47 +38,45 @@ function processSymbolData(str) {
     var result = '';
     var inSymbol = false;
     var nesting = 0;
-    for (var i = 0; i < str.length; i++) {
+    for(var i=0; i<str.length;i++) {
         if (str[i] === "[") {
-            if (nesting == 0) {
-                result += '<span class="symbol-data">';
-            } else {
-                result += str[i];
-            }
+            if (nesting == 0) { result += '<span class="symbol-data">'; }
+            else { result += str[i]; }
             nesting++;
             inSymbol = true;
-        } else if (str[i] === "]") {
+        }
+        else if (str[i] === "]") {
             nesting--;
 
-            if (nesting > 0) {
+            if(nesting > 0) {
                 result += str[i];
             } else if (inSymbol) {
                 result += '</span>';
                 inSymbol = false;
             }
-        } else {
+        }
+        else {
             result += str[i];
         }
     }
     return result.replace(/&lt;-|-&gt;/g, '');
 }
 
-$(document).ready(function () {
+$( document ).ready(function() {
     //If the first row is hidden, don't use header row styling
-    $('tr.hidden').each(function () {
+    $('tr.hidden').each(function() {
         $(this).next().addClass('slimRowColor0').removeClass('slimRowTitle');
     });
-    $(".test").each(function () {
+    $(".test").each(function() {
         $(this).before('<i class="fa fa-cog icon-suite" aria-hidden="true"></i>&nbsp;');
     });
-    $(".suite").each(function () {
+    $(".suite").each(function() {
         $(this).before('<i class="fa fa-cogs icon-test" aria-hidden="true" title="show/hide"></i>&nbsp;');
     });
-    $(".static").each(function () {
+    $(".static").each(function() {
         $(this).before('<i class="fa fa-file-o icon-static" aria-hidden="true"></i>&nbsp;');
     });
-
-    $('.contents li a').each(function () {
+    $('.contents li a').each(function() {
         var item = $(this)
         var orig = item.html();
         var tags = orig.match(/\((.*)\)/);
@@ -86,7 +84,7 @@ $(document).ready(function () {
             var nwhtml = orig.replace(/\(.*\)/, '');
             item.html(nwhtml);
             var tagList = tags[1].split(', ');
-            $.each(tagList, function (i, tag) {
+            $.each(tagList, function(i, tag){
                 var tagbadge = document.createElement("span");
                 tagbadge.setAttribute("class", "tag");
                 tagbadge.innerText = tag;
@@ -102,32 +100,32 @@ $(document).ready(function () {
     });
 
     //Do not use jQuery, as it rebuilds dom elements, breaking the failure nav
+
     [].forEach.call(document.getElementsByTagName('td'), cell => {
-        if (cell.innerHTML.match(/((?![^<>]*>)\$[\w]+=?)/g)) {
-            cell.innerHTML = cell.innerHTML.replace(/((?![^<>]*>)\$[\w]+=?)/g, '<span class="page-variable">$1</span>');
+        if(cell.innerHTML.match(/((?![^<>]*>)\$[\w]+=?)/g)) {
+            cell.innerHTML = cell.innerHTML.replace(/((?![^<>]*>)\$[\w]+=?)/g,'<span class="page-variable">$1</span>');
         }
-        if (cell.innerHTML.match(/(\$`.+`)/g)) {
+        if(cell.innerHTML.match(/(\$`.+`)/g)) {
             cell.innerHTML = cell.innerHTML.replace(/(\$`.+`)/g, '<span class="page-expr">$1</span>');
         }
     });
 
-    if (getCookie('collapseSymbols') == 'true') {
-        $("td").contents().filter(function () {
-            return this.nodeType == 3 && this.nodeValue.indexOf('->') >= 0 | this.nodeValue.indexOf('<-') >= 0;
-        })
-            .each(function (cell) {
+    if(getCookie('collapseSymbols') == 'true') {
+        $("td").contents().filter(function() {
+            return this.nodeType == 3 && this.nodeValue.indexOf('->') >= 0 | this.nodeValue.indexOf('<-') >= 0; })
+            .each( function(cell) {
                 if (this.parentNode != null && this.parentNode != undefined) {
                     this.parentNode.innerHTML = processSymbolData(this.parentNode.innerHTML);
                 }
             });
 
-        $('.symbol-data').prev('.page-variable, .page-expr').each(function () {
+        $('.symbol-data').prev('.page-variable, .page-expr').each(function() {
             $(this).addClass('canToggle');
             $(this).addClass('closed');
         });
 
-        $('.canToggle').click(function () {
-            if ($(this).hasClass('closed')) {
+        $('.canToggle').click(function() {
+            if($(this).hasClass('closed')) {
                 $(this).next('.symbol-data').css('display', 'inline-flex');
                 $(this).removeClass('closed');
                 $(this).addClass('open');
@@ -140,45 +138,45 @@ $(document).ready(function () {
     }
 
 
-    $('#alltags').change(function () {
-        if (this.checked) {
+    $('#alltags').change(function() {
+        if(this.checked) {
             $("#filtertags").attr('name', 'runTestsMatchingAllTags');
         } else {
             $("#filtertags").attr('name', 'runTestsMatchingAnyTag');
         }
     });
 
-    $('.fa-cogs').click(function () {
+    $('.fa-cogs').click(function() {
         $(this).siblings('ul').toggle();
     });
 
-    $('body').on('click', '#prefs-switch', function (e) {
+    $('body').on('click', '#prefs-switch', function(e) {
             e.preventDefault();
             $('.settings-panel').toggle();
         }
     );
 
-    $('body').on('click', '#theme-switch', function (e) {
+    $('body').on('click', '#theme-switch', function(e) {
             e.preventDefault();
             switchTheme();
         }
     );
 
-    $('body').on('click', '#collapse-switch', function (e) {
+    $('body').on('click', '#collapse-switch', function(e) {
             e.preventDefault();
             switchCollapse();
         }
     );
 
-    $('body').on('click', '#autoSave-switch', function (e) {
+    $('body').on('click', '#autoSave-switch', function(e) {
             e.preventDefault();
             switchAutoSave();
         }
     );
 
 
-    $('body').on('click', '.coll', function () {
-        if ($(this).children("input").is(":checked")) {
+    $('body').on('click', '.coll', function() {
+        if($(this).children("input").is(":checked")) {
             $(this).removeClass("closed");
             $(this).addClass("open");
         } else {
@@ -188,21 +186,21 @@ $(document).ready(function () {
     });
 
     function switchTheme() {
-        if (getCookie('themeType') == 'bootstrap-plus-dark') {
+        if(getCookie('themeType') == 'bootstrap-plus-dark') {
             document.cookie = "themeType=bootstrap-plus";
-            $('link[href="/files/fitnesse/bootstrap-plus/css/fitnesse-bootstrap-plus-dark.css"]').attr('href', '/files/fitnesse/bootstrap-plus/css/fitnesse-bootstrap-plus.css');
+            $('link[href="/files/fitnesse/bootstrap-plus/css/fitnesse-bootstrap-plus-dark.css"]').attr('href','/files/fitnesse/bootstrap-plus/css/fitnesse-bootstrap-plus.css');
             $('#theme-switch').removeClass('fa-toggle-on');
             $('#theme-switch').addClass('fa-toggle-off');
         } else {
             document.cookie = "themeType=bootstrap-plus-dark";
-            $('link[href="/files/fitnesse/bootstrap-plus/css/fitnesse-bootstrap-plus.css"]').attr('href', '/files/fitnesse/bootstrap-plus/css/fitnesse-bootstrap-plus-dark.css');
+            $('link[href="/files/fitnesse/bootstrap-plus/css/fitnesse-bootstrap-plus.css"]').attr('href','/files/fitnesse/bootstrap-plus/css/fitnesse-bootstrap-plus-dark.css');
             $('#theme-switch').removeClass('fa-toggle-off');
             $('#theme-switch').addClass('fa-toggle-on');
         }
     }
 
     function switchCollapse() {
-        if (getCookie('collapseSymbols') == 'true') {
+        if(getCookie('collapseSymbols') == 'true') {
             document.cookie = "collapseSymbols=false";
             $('#collapse-switch').removeClass('fa-toggle-on');
             $('#collapse-switch').addClass('fa-toggle-off');
@@ -214,7 +212,7 @@ $(document).ready(function () {
     }
 
     function switchAutoSave() {
-        if (getCookie('autoSave') == 'true') {
+        if(getCookie('autoSave') == 'true') {
             document.cookie = "autoSave=false";
             $('#autoSave-switch').removeClass('fa-toggle-on');
             $('#autoSave-switch').addClass('fa-toggle-off');
@@ -336,4 +334,3 @@ function postTag(currentURL, tagList, inputTag) {
         }
     });
 }
-
