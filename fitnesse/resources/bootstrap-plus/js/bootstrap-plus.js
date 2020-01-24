178632ed272d3c3229a89b514a8a1dcb8169b12d
Alex <s1129351@student.windesheim.nl>
2020-01-24 12:46:48 +0100

    undo Reformat code 4.9.2

--- fitnesse/resources/bootstrap-plus/js/bootstrap-plus.js
+++ fitnesse/resources/bootstrap-plus/js/bootstrap-plus.js
@@ -25,11 +25,11 @@ var parseCookies = function () {
 	var cookieData = (typeof document.cookie === 'string' ? document.cookie : '').trim();
 
       return (cookieData ? cookieData.split(';') : []).reduce(function (cookies, cookieString) {
-            var cookiePair = cookieString.split('=');
+              var cookiePair = cookieString.split('=');
 
-            cookies[cookiePair[0].trim()] = cookiePair.length > 1 ? cookiePair[1].trim() : '';
+              cookies[cookiePair[0].trim()] = cookiePair.length > 1 ? cookiePair[1].trim() : '';
 
-            return cookies;
+              return cookies;
       }, {});
 };
 
