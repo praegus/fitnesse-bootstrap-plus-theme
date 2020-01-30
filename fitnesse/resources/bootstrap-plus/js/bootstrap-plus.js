012882b714a37459b87a6627bff9b472ff4474b8
alex pennings <apennings@roc-dev.com>
2020-01-30 13:09:06 +0100

    made table generation unit-testable

--- fitnesse/resources/bootstrap-plus/js/bootstrap-plus.js
+++ fitnesse/resources/bootstrap-plus/js/bootstrap-plus.js
@@ -216,5 +216,46 @@ $( document ).ready(function() {
                 $('#autoSave-switch').addClass('fa-toggle-on');
             }
         }
+        generateVersionTable("/?testHistory&format=sorted");
+    console.log("/?testHistor&format=sorted");
 });
 
+function generateVersionTable(url) {
+    $.get(url, function (data) {
+        var check = document.getElementById("testHistoryTable");
+        if (check != undefined){
+            var parser = new DOMParser();
+            var parserhtml = parser.parseFromString(data, 'text/html');
+            var table = parserhtml.getElementsByTagName("table")[0];
+            var rows = table.getElementsByTagName("tr");
+            var resultsReportTd = rows[0].childNodes[9];
+            // set colspan
+            resultsReportTd.innerText = "Last 5 Results";
+            resultsReportTd.setAttribute("colspan",5);
+
+            if(rows.length > 5){
+                var rowNumberToSlice = rows.length - 5;
+                $(rows,"tr").slice(-rowNumberToSlice).remove();
+            }
+            for (var i=1;i<rows.length;i++){
+                var cells = rows[i].getElementsByTagName("td");
+                if (cells.length > 9){
+                    var colNumberToSlice = cells.length - 9;
+                    $(cells,"td").slice(-colNumberToSlice).remove();
+                }
+            }
+
+
+            check.appendChild(table);
+        }
+        return table;
+
+
+    })
+}
+try {
+    module.exports = {
+        generateVersionTable: generateVersionTable
+    };
+} catch (e) {
+}
