043213f75744c7d8eb4055a46f9f68d3921386b4
alex pennings <apennings@roc-dev.com>
2020-02-04 13:41:37 +0100

    opzimized unit testing, documented code

--- fitnesse/resources/jest/Versiecheck.test.js
+++ fitnesse/resources/jest/Versiecheck.test.js
@@ -1,11 +1,10 @@
 jest.mock('jquery');
+beforeEach(() => jest.resetModules());
+//define dummy data
+const dummyData = require('./mockup-data/versionCheck/defaultDummyData');
+const toolchainDummyData = require('./mockup-data/versionCheck/toolchainDummyData');
 
-describe('versiecheck', function () {
-    const $ = require('jquery');
-    const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
-    var dummyData = [ {"groupid": "nl.praegus", "artifactid": "toolchain-fixtures", "currentVersion": "1.16", "latest": "1.19"}];
-    var toolchainDummyData =  [{"groupid": "nl.praegus", "artifactid": "toolchain-fitnesse-plugin", "version": "1.20-SNAPSHOT", "latest": "1.20"}];
-test('Test if $.ajax has the correct params', () => {
+it('Test if $.ajax has the correct params', () => {
     const dummyCallback = () => {
     };
     const expectedResult = {
@@ -16,42 +15,48 @@ test('Test if $.ajax has the correct params', () => {
         error: expect.any(Function)
     };
     //MAKE CONST URL
-    jsfile.getVersionData(dummyCallback,'http://localhost:9090/?mavenVersions');
+    const $ = require('jquery');
+    const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
+    jsfile.getVersionData(dummyCallback, 'http://localhost:9090/?mavenVersions');
 
     // Now make sure that $.ajax was properly called
     expect($.ajax).toBeCalledWith(expectedResult);
 });
-test('Correct toolchain version property correction',()=>{
-console.log(jsfile.versionCheck(toolchainDummyData));
-    expect(jsfile.versionCheck(toolchainDummyData)).toEqual(expect.objectContaining({currentVersion:"1.20-SNAPSHOT"}));
+it('if currentversion property has an irregular key, correct it', () => {
+    const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
+
+    expect(jsfile.versionCheck(toolchainDummyData)).toEqual(expect.objectContaining({currentVersion: '1.20-SNAPSHOT'}));
 });
 
-test('correct current version data formatting',()=>{
+it('parse current version string to int correctly', () => {
+    const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
 
-expect(jsfile.versionCheck(dummyData)).toEqual(expect.objectContaining({formatCurrentVersion:116}));
+    expect(jsfile.versionCheck(dummyData)).toEqual(expect.objectContaining({formatCurrentVersion: 116}));
 });
 
-test('correct latest version data formatting',()=>{
+it('parse latest version string to int correctly', () => {
+    const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
 
-expect(jsfile.versionCheck(dummyData)).toEqual(expect.objectContaining({formatLatestVersion:119}));
+    expect(jsfile.versionCheck(dummyData)).toEqual(expect.objectContaining({formatLatestVersion: 119}));
 });
 
-test('if version is outdated expect status to outdated',()=>{
+it('if version is outdated expect status to outdated', () => {
+    const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
 
-expect(jsfile.versionCheck(dummyData).status).toBe("Outdated");
+    expect(jsfile.versionCheck(dummyData).status).toBe('Outdated');
 });
 
-    test('if version is up to date expect status to up to date',()=>{
+it('if version is up to date expect status to up to date', () => {
+    const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
 
-        expect(jsfile.versionCheck(toolchainDummyData).status).toBe("Up-to-date");
-    });
+    expect(jsfile.versionCheck(toolchainDummyData).status).toBe('Up-to-date');
+});
 
-    test('if version is ahead expect status to ahead',()=>{
+it('if version is ahead expect status to ahead', () => {
+    const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
 
+ // take latest version, parse to int, add one, parse to string and send it to versioncheck to create the "ahead" state and check it
+    dummyData[0].currentVersion = String(parseInt(dummyData[0].latest.replace(/\D/g, "")) + 1);
+    expect(jsfile.versionCheck(dummyData).status).toBe("Ahead");
 
-        dummyData[0].currentVersion = parseInt(dummyData[0].latest.replace(/\D/g, ""))+1;
-console.log(dummyData);
-        expect(jsfile.versionCheck(dummyData).status).toBe("Outdated");
-
-    });
 });
