664dee30a02c0f6de429f30cccc0c25e138450b6
Caitlin <Caitlincoopmans@gmail.com>
2020-02-05 09:57:14 +0100

    Optimized test code

--- fitnesse/resources/jest/TestHistoryChecker.test.js
+++ fitnesse/resources/jest/TestHistoryChecker.test.js
@@ -6,8 +6,7 @@ beforeEach(() => jest.resetModules());
 it('Test if $.ajax has the correct params', () => {
     const $ = require('jquery');
     const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
-    const dummyCallback = () => {
-    };
+    const dummyCallback = () => {};
     const expectedResult = {
         type: 'GET',
         url: expect.stringContaining('?testHistory&format=sorted'),
@@ -15,6 +14,7 @@ it('Test if $.ajax has the correct params', () => {
         success: expect.any(Function),
         error: expect.any(Function)
     };
+
     //MAKE CONST URL
     jsfile.getPageHistory('http://localhost:9090/?testHistory&format=sorted', dummyCallback);
 
@@ -22,7 +22,8 @@ it('Test if $.ajax has the correct params', () => {
     expect($.ajax).toBeCalledWith(expectedResult);
 });
 
-it('Test rows', () => {
+
+it('Test if rows are no longer than 5', () => {
     const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
     const dataFile = require('./mockup-data/TestHistoryData');
     const neededHTML = '<div id="testHistoryTable"></div>';
@@ -34,6 +35,7 @@ it('Test rows', () => {
 
     expect(receivedResult).toEqual(expectedResult);
 });
+
 
 it('Test if "Last 5 Result" column has the correct 5 cells', () => {
     const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
