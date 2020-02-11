d76473dc225fe8dcfee822ef70f48ce17592cad5
alex pennings <apennings@roc-dev.com>
2020-02-11 16:42:26 +0100

    combined semantic versioning and litteral string comparison and added tests

--- fitnesse/resources/bootstrap-plus/js/bootstrap-plus.js
+++ fitnesse/resources/bootstrap-plus/js/bootstrap-plus.js
@@ -672,7 +672,7 @@ START VERSIONCHECKER
  */
 
 function getVersionData(callback, url) {
-  // const $ = require('jquery');
+   //const $ = require('jquery');
     $.ajax({
         type: 'GET',
         url: url,
@@ -692,11 +692,36 @@ function versionCheck(data) {
                 versionData["currentVersion"] = versionData['version'];
                 delete versionData['version'];
             }
-            //compare strings, Replace -SNAPSHOT if found
-            if (versionData.currentVersion.replace('-SNAPSHOT','') !== versionData.latest){
-                versionData['status'] = 'Outdated';
-            }else{
-                versionData['status'] = 'Up-to-date';
+
+            //try to use semantic versioning as logic, if it runs into an error use litteral string comparison
+            try{
+                // split version strings by dot and parse them to ints
+                let semanticCurrentVersion = versionData.currentVersion.replace('-SNAPSHOT', '').split('.').map(Number);
+                let semanticLatestVersion = versionData.latest.replace('-SNAPSHOT', '').split('.').map(Number);
+                // make arrays equal in length if necessary so there wont be an undefined index
+                if (semanticCurrentVersion.length < semanticLatestVersion.length || semanticLatestVersion.length < semanticCurrentVersion.length) {
+                    while (semanticCurrentVersion.length < semanticLatestVersion.length) semanticCurrentVersion.push(0);
+                    while (semanticLatestVersion.length < semanticCurrentVersion.length) semanticLatestVersion.push(0);
+                }
+                semanticLatestVersion.forEach(function (semanticLatestVersionNumber, i) {
+                    //check if current ver is smaller then the latest and check if status is not defined so it doesnt have to loop more than it has to
+                    if (versionData.status === undefined) {
+                        if (semanticLatestVersionNumber < semanticCurrentVersion[i]) {
+                            versionData['status'] = 'Ahead';
+                        } else if (semanticCurrentVersion[i] < semanticLatestVersionNumber && i !== semanticLatestVersion.length) {
+                            versionData['status'] = 'Outdated';
+                        } else if (semanticCurrentVersion[i] === semanticLatestVersionNumber && i === semanticLatestVersion.length - 1) {
+                            versionData['status'] = 'Up-to-date';
+                        }
+                    }
+                })}
+            catch (e) {
+                //compare strings, Replace -SNAPSHOT if found
+                if (versionData.currentVersion.replace('-SNAPSHOT','') !== versionData.latest){
+                    versionData['status'] = 'Outdated';
+                }else{
+                    versionData['status'] = 'Up-to-date';
+                }
             }
 
             // Place in html
--- fitnesse/resources/jest/Versiecheck.test.js
+++ fitnesse/resources/jest/Versiecheck.test.js
@@ -1,7 +1,6 @@
 jest.mock('jquery');
 beforeEach(() => jest.resetModules());
 //define dummy data
-
 it('Test if $.ajax has the correct params', () => {
     //MAKE CONST URL
     const $ = require('jquery');
@@ -15,22 +14,16 @@ it('Test if $.ajax has the correct params', () => {
         success: expect.any(Function),
         error: expect.any(Function)
     };
-
     jsfile.getVersionData(dummyCallback, 'http://localhost:9090/?mavenVersions');
-
     // Now make sure that $.ajax was properly called
     expect($.ajax).toBeCalledWith(expectedResult);
 });
-
 it('if currentversion property has an irregular key, expect corrected format', () => {
     const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
     const toolchainDummyData = require('./mockup-data/versionCheck/toolchainDummyData');
     const neededHTML = '<table id="versioncheck"></table>';
-
     document.body.innerHTML = neededHTML;
-
     jsfile.versionCheck(toolchainDummyData);
-
     const receivedResult = document.getElementById('versioncheck').innerHTML;
     const expectedResult =
         '<tr class="check">' +
@@ -39,19 +32,14 @@ it('if currentversion property has an irregular key, expect corrected format', (
         '<td><p>1.20</p></td>' +
         '<td class="Up-to-date"><p>Up-to-date</p></td>' +
         '</tr>';
-
     expect(receivedResult).toMatch(expectedResult);
 });
-
 it('if version is outdated expect status to outdated', () => {
     const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
     const dummyData = require('./mockup-data/versionCheck/defaultDummyData');
     const neededHTML = '<table id="versioncheck"></table>';
-
     document.body.innerHTML = neededHTML;
-
     jsfile.versionCheck(dummyData);
-
     const receivedResult = document.getElementById('versioncheck').innerHTML;
     const expectedResult =
         '<tr class="check">' +
@@ -60,20 +48,15 @@ it('if version is outdated expect status to outdated', () => {
         '<td><p>1.19</p></td>' +
         '<td class="Outdated"><p>Outdated</p></td>' +
         '</tr>';
-
     expect(receivedResult).toMatch(expectedResult);
 });
-
 it('if version is up to date expect status to up to date', () => {
     const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
     let dummyData = require('./mockup-data/versionCheck/defaultDummyData');
     const neededHTML = '<table id="versioncheck"></table>';
-
     document.body.innerHTML = neededHTML;
     dummyData[0].currentVersion = dummyData[0].latest;
-
     jsfile.versionCheck(dummyData);
-
     const receivedResult = document.getElementById('versioncheck').innerHTML;
     const expectedResult =
         '<tr class="check">' +
@@ -82,21 +65,126 @@ it('if version is up to date expect status to up to date', () => {
         '<td><p>1.19</p></td>' +
         '<td class="Up-to-date"><p>Up-to-date</p></td>' +
         '</tr>';
+    expect(receivedResult).toMatch(expectedResult);
+});
+
+it('if version is ahead expect status to ahead', () => {
+    const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
+    let dummyData = require('./mockup-data/versionCheck/defaultDummyData');
+    const neededHTML = '<table id="versioncheck"></table>';
+
+    document.body.innerHTML = neededHTML;
+    dummyData[0].currentVersion = '1.20';
+
+
+    jsfile.versionCheck(dummyData);
+
+    const receivedResult = document.getElementById('versioncheck').innerHTML;
+    const expectedResult =
+        '<tr class="check">' +
+        '<td><p>toolchain fixtures</p></td>' +
+        '<td><p>1.20</p></td>' +
+        '<td><p>1.19</p></td>' +
+        '<td class="Ahead"><p>Ahead</p></td>' +
+        '</tr>';
 
     expect(receivedResult).toMatch(expectedResult);
+
 });
 
 it('if data is null expect table to be empty', () => {
     const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
     const neededHTML = "<table id='versioncheck'></table>";
-
     document.body.innerHTML = neededHTML;
     jsfile.versionCheck();
-
     const receivedResult = document.getElementById('versioncheck').innerHTML;
-
     expect(receivedResult).toBe('');
 });
 
+it('currentversion only has a major version and is higher and should return ahead', () => {
+    const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
+    let dummyData = require('./mockup-data/versionCheck/defaultDummyData');
+    const neededHTML = '<table id="versioncheck"></table>';
 
+    document.body.innerHTML = neededHTML;
+    dummyData[0].currentVersion = '2';
 
+    jsfile.versionCheck(dummyData);
+
+    const receivedResult = document.getElementById('versioncheck').innerHTML;
+    const expectedResult =
+        '<tr class="check">' +
+        '<td><p>toolchain fixtures</p></td>' +
+        '<td><p>2</p></td>' +
+        '<td><p>1.19</p></td>' +
+        '<td class="Ahead"><p>Ahead</p></td>' +
+        '</tr>';
+
+    expect(receivedResult).toMatch(expectedResult);
+});
+
+it('latestversion only has a major version and is higher and should return outdated', () => {
+    const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
+    let dummyData = require('./mockup-data/versionCheck/defaultDummyData');
+    const neededHTML = '<table id="versioncheck"></table>';
+
+    document.body.innerHTML = neededHTML;
+    dummyData[0].latest = '2';
+
+    jsfile.versionCheck(dummyData);
+
+    const receivedResult = document.getElementById('versioncheck').innerHTML;
+    const expectedResult =
+        '<tr class="check">' +
+        '<td><p>toolchain fixtures</p></td>' +
+        '<td><p>1.16</p></td>' +
+        '<td><p>2</p></td>' +
+        '<td class="Outdated"><p>Outdated</p></td>' +
+        '</tr>';
+
+    expect(receivedResult).toMatch(expectedResult);
+});
+
+it('if versions contain letter switch to string comparison and return up-to-date', () => {
+    const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
+    const cornerCaseDummyData = require('./mockup-data/versionCheck/cornerCaseDummyData');
+    const neededHTML = '<table id="versioncheck"></table>';
+
+    document.body.innerHTML = neededHTML;
+
+    jsfile.versionCheck(cornerCaseDummyData);
+
+    const receivedResult = document.getElementById('versioncheck').innerHTML;
+    const expectedResult =
+        '<tr class="check">' +
+        '<td><p>google api services gmail</p></td>' +
+        '<td><p>v1-rev102-1.25.0</p></td>' +
+        '<td><p>v1-rev102-1.25.0</p></td>' +
+        '<td class="Up-to-date"><p>Up-to-date</p></td>' +
+        '</tr>';
+
+    expect(receivedResult).toMatch(expectedResult);
+});
+
+it('if versions contain letter switch to string comparison and return outdated', () => {
+    const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
+    let cornerCaseDummyData = require('./mockup-data/versionCheck/cornerCaseDummyData');
+    const neededHTML = '<table id="versioncheck"></table>';
+
+    document.body.innerHTML = neededHTML;
+
+    cornerCaseDummyData[0].latest = "v1-rev102-1.25.1";
+
+    jsfile.versionCheck(cornerCaseDummyData);
+
+    const receivedResult = document.getElementById('versioncheck').innerHTML;
+    const expectedResult =
+        '<tr class="check">' +
+        '<td><p>google api services gmail</p></td>' +
+        '<td><p>v1-rev102-1.25.0</p></td>' +
+        '<td><p>v1-rev102-1.25.1</p></td>' +
+        '<td class="Outdated"><p>Outdated</p></td>' +
+        '</tr>';
+
+    expect(receivedResult).toMatch(expectedResult);
+});
