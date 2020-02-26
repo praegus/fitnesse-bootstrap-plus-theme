f5a75d9739e6a09f78999ec0a64a6654b4687507
DeOfficieleAlex <apennings@roc-dev.com>
2020-02-26 09:30:20 +0100

    Toolchain bug 2 - $.ajax testing with jest fix (#30)
    Remove jquery includes

--- fitnesse/resources/jest/TestHistoryChecker.test.js
+++ fitnesse/resources/jest/TestHistoryChecker.test.js
@@ -2,27 +2,6 @@ jest.mock('jquery');
 
 beforeEach(() => jest.resetModules());
 
-// const $ = require('jquery');
-it('Test if $.ajax has the correct params', () => {
-    const $ = require('jquery');
-    const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
-    const dummyCallback = () => {};
-    const expectedResult = {
-        type: 'GET',
-        url: expect.stringContaining('?recentTestHistory'),
-        contentType: 'charset=utf-8',
-        success: expect.any(Function),
-        error: expect.any(Function)
-    };
-
-    //MAKE CONST URL
-    jsfile.getPageHistory('http://localhost:9090/?recentTestHistory', dummyCallback);
-
-    // Now make sure that $.ajax was properly called
-    expect($.ajax).toBeCalledWith(expectedResult);
-});
-
-
 it('Test if rows are no longer than 5', () => {
     const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
     const dataFile = require('./mockup-data/TestHistoryData');
@@ -35,7 +14,6 @@ it('Test if rows are no longer than 5', () => {
 
     expect(receivedResult).toEqual(expectedResult);
 });
-
 
 it('Test if "Last 5 Result" column has the correct 5 cells', () => {
     const jsfile = require('../bootstrap-plus/js/bootstrap-plus');
